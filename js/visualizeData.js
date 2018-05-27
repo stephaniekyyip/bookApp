6866d224dedf0915d6592ee4440f6d8949fabad6
Stephanie Yip <stephaniekyyip@gmail.com>
2018-05-27 00:03:15 -1000

    Testing D3.js for data visualization.

---
+++ js/visualizeData.js
@@ -0,0 +1,13 @@
+console.log("visualdata js");
+var data = [24,20,26,24,21,25,9,8,1,6,3];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
