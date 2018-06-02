bd3d14e888a794ca1333dd358e30dfefad4a268c
Stephanie Yip <stephaniekyyip@gmail.com>
2018-06-01 21:21:26 -1000

    Switched to C3.js for chart. Done with initial chart design.

--- js/visualizeData.js
+++ js/visualizeData.js
@@ -1,13 +1,54 @@
-console.log("visualdata js");
-var data = [24,20,26,24,21,25,9,8,1,6,3];
+var myData = [];
+var numBooks= [24,20,26,24,21,25,9,8,1,6,3];
+var year = [2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+for(var i = 0; i < numBooks.length; i++ ){
+  myData.push({
+    numBooks: numBooks[i],
+    year: year[i]
+  });
+}
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+var xLabel = "year";
+var yLabel = "numBooks";
+var chartTitle = "Total Books Read Each Year";
+
+var chart = c3.generate({
+    data: {
+        json: myData,
+        keys:{
+          x: xLabel,
+          y: yLabel,
+          value: [xLabel, yLabel]
+        },
+        type: 'bar',
+        labels: true
+    },
+    bar: {
+        width: {
+            ratio: 0.7 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    },
+     legend: {
+       show: false
+     },
+     axis:{
+       x: {
+         type: 'category'
+       },
+       y: {
+         show: false
+       }
+     },
+     tooltip:{
+       show: false
+     },
+     title: {
+       text: chartTitle
+     },
+     padding: {
+       top: 30,
+     }
+});
