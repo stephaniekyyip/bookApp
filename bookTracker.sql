7ccc66098629f5d22e4892b065dd96b312cb5878
Stephanie Yip <stephaniekyyip@gmail.com>
2018-06-14 20:17:18 -1000

    New heroku cleardb config

---
+++ bookTracker.sql
@@ -0,0 +1,61 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.7.22)
+# Database: bookApp
+# Generation Time: 2018-06-15 05:51:11 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table book_list
+# ------------------------------------------------------------
+
+CREATE TABLE `book_list` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `author_first` varchar(30) NOT NULL,
+  `author_last` varchar(30) NOT NULL,
+  `year_read` int(4) NOT NULL,
+  `year_pub` int(4) DEFAULT NULL,
+  `num_pgs` int(11) DEFAULT NULL,
+  `for_class` tinyint(1) DEFAULT NULL,
+  `reread` tinyint(1) DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL DEFAULT '',
+  `name` varchar(256) NOT NULL DEFAULT '',
+  `pwd` varchar(256) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/database.php
+++ php/database.php
@@ -8,10 +8,19 @@
 // ----------------------------------------------------------------------------
 
   class Database{
-    private $servername = "localhost";
-    private $username = "root";
-    private $db_name  = "bookApp";
-    private $password = "";
+    //mySQL DB connection for localhost
+    // private $servername = "localhost";
+    // private $username = "root";
+    // private $db_name  = "bookApp";
+    // private $password = "";
+
+    //mySQL DB connection for heroku via ClearDB
+    private $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    private $servername = $cleardb_url["host"];
+    private $username = $cleardb_url["user"];
+    private $password = $cleardb_url["pass"];
+    private $db_name = substr($cleardb_url["path"],1);
+
     public $conn;
 
     public function connectToDatabase(){
