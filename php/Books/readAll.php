e2dad176571b83443abcd7e281747fda3491506f
Stephanie Yip <stephaniekyyip@gmail.com>
2018-06-14 22:00:32 -1000

    Removing database class

--- php/Books/readAll.php
+++ php/Books/readAll.php
@@ -12,8 +12,8 @@
   require_once ('books.php');
 
   //mysql connection
-  $database = new Database();
-  $conn = $database->connectToDatabase();
+  // $database = new Database();
+  // $conn = $database->connectToDatabase();
 
   $bookList = new Books($conn);
 
